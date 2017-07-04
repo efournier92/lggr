d94ef2cc8c3811a634c1096d3fd3723244e10a34
efournier92 <efournier92@gmail.com>
2017-07-04 12:22:06 -0400

    finish log-printer logic

--- log-builder.rb
+++ log-builder.rb
@@ -8,3 +8,28 @@ year = gets.chomp
 
 day = firstSunday
 
+52.times do
+
+  date = "#{ year }-01-#{ '%02d' % day }-#{ year }"
+
+  out_file.puts(
+    %{
+      *******************
+      #{ date }
+      *******************
+      Mon - Gt, Ln, 
+      Tues - Gt, Ln, 
+      Wed - Gt, Ln, 
+      Thu - Gt, GS(), Ln, 
+      Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
+      Sat - Gt, 
+      Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
+    }
+  )
+
+  day += 7
+
+end
+
+out_file.close
+
