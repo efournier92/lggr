7b5507526b466e0cbbe9a1867abe4ac50a1d005b
efournier92 <efournier92@gmail.com>
2017-07-05 20:38:50 -0400

    add get-first-sunday method logic

--- log-builder.rb
+++ log-builder.rb
@@ -1,35 +1,46 @@
 out_file = File.new("logYear.txt", "w")
 
-print "1st Sunday in January?\n>> "
-firstSunday = gets.chomp.to_i
-
 print "What Yeat?\n>> "
 year = gets.chomp
 
-day = firstSunday
+# 52.times do
 
-52.times do
+#   date = "#{ year }-01-#{ '%02d' % day }-#{ year }"
 
-  date = "#{ year }-01-#{ '%02d' % day }-#{ year }"
+#   out_file.puts(
+#     %{
+#       *******************
+#       #{ date }
+#       *******************
+#       Mon - Gt, Ln, 
+#       Tues - Gt, Ln, 
+#       Wed - Gt, Ln, 
+#       Thu - Gt, GS(), Ln, 
+#       Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
+#       Sat - Gt, 
+#       Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
+#     }
+#   )
 
-  out_file.puts(
-    %{
-      *******************
-      #{ date }
-      *******************
-      Mon - Gt, Ln, 
-      Tues - Gt, Ln, 
-      Wed - Gt, Ln, 
-      Thu - Gt, GS(), Ln, 
-      Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
-      Sat - Gt, 
-      Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
-    }
-  )
+#   day += 7
 
-  day += 7
+# end
 
+# out_file.close
+
+def getFirstSunday(year)
+    years_since = year - 1
+    leap_years = years_since / 4
+    century_years = years_since / 100
+    four_century_years = years_since / 400
+
+    
+    total_leap_years = leap_years - century_years + four_century_years
+    total_precesion = years_since + total_leap_years
+    day_index = total_precesion % 7
+    
+    first_sunday = 7 - day_index;
 end
 
-out_file.close
+binding.pry
 
