ae230de52249e78ef3d7d196b378d70871a15299
efournier92 <efournier92@gmail.com>
2017-07-07 18:24:49 -0400

    fix EOS multiline string formatting issue

--- log-builder.rb
+++ log-builder.rb
@@ -6,28 +6,28 @@ year = gets.chomp.to_i
 day = get_first_sunday(year)
 month = 1
 
-days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
-if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)) {
-    days_in_month[1] = 29;
-}
+if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
+    days_in_month[1] = 29
+end
 
   52.times do
 
     date = "#{ year }-#{ '%02d' % month }-#{ '%02d' % day }"
     out_file.puts(
-      ```
-*******************
-#{ date }
-*******************
-Mon - Gt, Ln, 
-Tues - Gt, Ln, 
-Wed - Gt, Ln, 
-Thu - Gt, GS(), Ln, 
-Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
-Sat - Gt, 
-Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
-```
+      <<~EOS
+        *******************
+        #{ date }
+        *******************
+        Mon - Gt, Ln, 
+        Tues - Gt, Ln, 
+        Wed - Gt, Ln, 
+        Thu - Gt, GS(), Ln, 
+        Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
+        Sat - Gt, 
+        Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
+      EOS
     )
 
     day += 7
@@ -38,21 +38,21 @@ Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap,
       month += 1
     end
 
-    end
+  end
 
-    out_file.close
+  out_file.close
 
-    def get_first_sunday(year)
-      years_since = year - 1
-      leap_years = years_since / 4
-      century_years = years_since / 100
-      four_century_years = years_since / 400
+  def get_first_sunday(year)
+    years_since = year - 1
+    leap_years = years_since / 4
+    century_years = years_since / 100
+    four_century_years = years_since / 400
 
 
-      total_leap_years = leap_years - century_years + four_century_years
-      total_precesion = years_since + total_leap_years
-      day_index = total_precesion % 7
+    total_leap_years = leap_years - century_years + four_century_years
+    total_precesion = years_since + total_leap_years
+    day_index = total_precesion % 7
 
-      first_sunday = 7 - day_index;
-    end
+    first_sunday = 7 - day_index;
+  end
 
