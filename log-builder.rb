4697336f5db83f6643215ff3d8ad52f17d9a17c5
efournier92 <efournier92@gmail.com>
2017-07-07 18:25:07 -0400

    fix extra return formatting issue

--- log-builder.rb
+++ log-builder.rb
@@ -3,45 +3,6 @@ out_file = File.new("logYear.txt", "w")
 print "What Year?\n>> "
 year = gets.chomp.to_i
 
-day = get_first_sunday(year)
-month = 1
-
-days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
-
-if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
-    days_in_month[1] = 29
-end
-
-  52.times do
-
-    date = "#{ year }-#{ '%02d' % month }-#{ '%02d' % day }"
-    out_file.puts(
-      <<~EOS
-        *******************
-        #{ date }
-        *******************
-        Mon - Gt, Ln, 
-        Tues - Gt, Ln, 
-        Wed - Gt, Ln, 
-        Thu - Gt, GS(), Ln, 
-        Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
-        Sat - Gt, 
-        Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, 
-      EOS
-    )
-
-    day += 7
-    total_precesion += 7
-
-    if day > days_in_month(month)
-      day = days_in_month[month] - day
-      month += 1
-    end
-
-  end
-
-  out_file.close
-
   def get_first_sunday(year)
     years_since = year - 1
     leap_years = years_since / 4
@@ -55,4 +16,43 @@ end
 
     first_sunday = 7 - day_index;
   end
+
+total_precesion = 0
+day = get_first_sunday(year)
+month = 1
+
+days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
+    days_in_month[1] = 29
+end
+
+  52.times do
+
+    date = "#{ year }-#{ '%02d' % month }-#{ '%02d' % day }"
+    out_file.print(
+    %{ 
+*******************
+#{ date }
+*******************
+Mon - Gt, Ln, 
+Tues - Gt, Ln, 
+Wed - Gt, Ln, 
+Thu - Gt, GS(), Ln, 
+Fri - Gt, LgWks, aLg, Lg, Bgt, PyCC, 
+Sat - Gt, 
+Sun - Gt, Amz(), ClHm(), ClnKtch, ClnFrdg, Vac(), Sv, Ns, AF(00), TM, Ln, Ap, }
+    )
+
+    day += 7
+    total_precesion += 7
+
+    if day > days_in_month[month]
+      day = days_in_month[month] - day
+      month += 1
+    end
+
+  end
+
+  out_file.close
 
