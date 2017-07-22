05031e5542e3519706ffd94591efd17dd3535546
efournier92 <efournier92@gmail.com>
2017-07-21 20:24:05 -0400

    add do & lg printer separate files

---
+++ cal_tools.rb
@@ -0,0 +1,32 @@
+module Cal_Tools
+  def self.get_first_sunday(year)
+    years_since = year - 1
+    leap_years = years_since / 4
+    century_years = years_since / 100
+    four_century_years = years_since / 400
+
+    total_leap_years = leap_years - century_years + four_century_years
+    total_precesion = years_since + total_leap_years
+    day_index = total_precesion % 7
+
+    first_sunday = 7 - day_index;
+  end
+
+  def self.get_first_friday(year)
+    first_sunday = get_first_sunday(year)
+    if first_sunday >= 2
+      first_friday = first_sunday - 2
+    else
+      first_friday = first_sunday + 5
+    end
+  end
+
+  def self.is_leap_year?(year)
+    if (year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0))
+      true 
+    else
+      false 
+    end
+  end
+end
+
--- lg_builder.rb
+++ lg_builder.rb
@@ -121,7 +121,7 @@ year = gets.chomp.to_i
 month = 1
 
 days_in_month = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
-if cal_tools.is_leap_year?(year)
+if Cal_Tools.is_leap_year?(year)
   days_in_month[1] = 29
 end
 
