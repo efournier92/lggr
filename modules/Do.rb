e3d66640b52f2f8fd373fbbb9915f57163d48b29
efournier92 <efournier92@gmail.com>
2017-09-09 19:23:32 -0400

    fix do_week template objects syntax

--- modules/Do.rb
+++ modules/Do.rb
@@ -3,6 +3,7 @@ module Do
     out_file = File.new("./Out/#{ year }_DO.txt", "w")
     day = Cal_Tools.get_first_monday(year)
 
+    do_year = []
     week_index = 0
 
     52.times do
@@ -13,37 +14,46 @@ module Do
         do_week['Sun'] = Templates.do_week_odd_sunday
       end
 
+      do_week.each_with_index do |day, day_details|
+        day_details.month = month
+        day_details.day = do_week.days_this_week[index]
+      end
+
       do_week.days = Cal_Tools.days_this_week(day, month)
 
       do_week = Cal_Tools.is_week_odd?(do_week, week_index)
       do_week = Cal_Tools.month_end(do_week, do_week.days)
       do_week = Cal_Tools.month_start(do_week, week_index, do_week.days, month)
 
-      out_file.puts('************************')
-      out_file.puts("#{ year }-#{ '%02d' % month }-#{ '%02d' % day }")
-      out_file.puts('************************')
-
-      do_week.each do |day, tasks|
-        out_file.print(
-          day + ' - '
-        )
-
-        tasks.each do |task|
-          out_file.print (
-            task + ', '
-          )
-        end
-        out_file.print("\n")
-      end
-
-      day += 7
-      if day > days_in_month[month - 1]
-        day = day - days_in_month[month - 1]
-        month += 1
-      end
-      first_day_of_month = nil
-      week_index += 1
+      do_year.push(do_week)
     end
+    binding.pry
+
+      # out_file.puts('************************')
+      # out_file.puts("#{ year }-#{ '%02d' % month }-#{ '%02d' % day }")
+      # out_file.puts('************************')
+
+      # do_week.each do |day, tasks|
+      #   out_file.print(
+      #     day + ' - '
+      #   )
+
+      #   tasks.each do |task|
+      #     out_file.print (
+      #       task + ', '
+      #     )
+      #   end
+      #   out_file.print("\n")
+      # end
+
+      # day += 7
+      # if day > days_in_month[month - 1]
+      #   day = day - days_in_month[month - 1]
+      #   month += 1
+      # end
+      # first_day_of_month = nil
+      # week_index += 1
+    # end
 
     out_file.puts("************************\n\n")
     out_file.close
