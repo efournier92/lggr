1ac3da4c739fd8cce9c08e9aac3ac72dfc527fc7
efournier92 <efournier92@gmail.com>
2017-09-12 18:09:55 -0400

    add initialize method to Year model

--- modules/Do.rb
+++ modules/Do.rb
@@ -6,27 +6,6 @@ module Do
     do_year = []
     week_index = 0
 
-    52.times do
-      do_week = Templates.do_week
-      do_week.week_index = week_index
-
-      if week_index.odd?
-        do_week['Sun'] = Templates.do_week_odd_sunday
-      end
-
-      do_week.each_with_index do |day, day_details|
-        day_details.month = month
-        day_details.day = do_week.days_this_week[index]
-      end
-
-      do_week.days = Cal_Tools.days_this_week(day, month)
-
-      do_week = Cal_Tools.is_week_odd?(do_week, week_index)
-      do_week = Cal_Tools.month_end(do_week, do_week.days)
-      do_week = Cal_Tools.month_start(do_week, week_index, do_week.days, month)
-
-      do_year.push(do_week)
-    end
     binding.pry
 
       # out_file.puts('************************')
