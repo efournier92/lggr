94a6f8a3e9fb25d59a363643cffc8449152da5f2
efournier92 <efournier92@gmail.com>
2017-09-13 18:18:42 -0400

    add logic to Week class initializer

---
+++ Models/week.rb
@@ -0,0 +1,15 @@
+class Week
+  def initialize
+    @days = {}
+
+      {
+        'Mon' => { 'tasks' => ['Gt', 'Af_Rcv(1900)'] },
+        'Tue' => { 'tasks' => ['Gt'] },
+        'Wed' => { 'tasks' => ['Gt'] },
+        'Thu' => { 'tasks' => ['Gt'] },
+        'Fri' => { 'tasks' => ['Gt', 'aLg', 'Lg', 'FnLg[R]s', 'LgWk', 'Bgt', 'PyCC'] },
+        'Sat' => { 'tasks' => ['Gt', 'Af_Ord()'] },
+        'Sun' => { 'tasks' => ['Gt', 'Amz()', 'ClnMbDsktp', 'TM', 'Ln', 'ClHm(1300, )', 'Vac()', 'ClnKtch', 'ClnFrdg', 'Sv', 'Plk', 'Ns', 'Ap'] }
+    }
+  end
+
