d0176619f8ff24501f8ba3540604dc0f003370c6
efournier92 <efournier92@gmail.com>
2017-09-18 17:54:41 -0400

    translate SQL to ruby in easter method

--- LGGR.rb
+++ LGGR.rb
@@ -11,9 +11,21 @@ type = ''
 month = 1
 do_year = Year.new(1999)
 
-def easter(do_year, do_year.year)
-  
+def easter(year)
+   epact_calc = ( 24 + 19 * ( year % 19 ) ) % 30
+    paschal_days = epact_calc - ( epact_calc / 28 )
+    days_to_sunday = paschal_days - (
+        ( year + ( year / 4 ) + paschal_days - 13 ) % 7
+    )
+    easter_month = 3 + ( days_to_sunday + 40 ) / 44
+    easter_day = days_to_sunday + 28 - (
+        31 * ( easter_month / 4 )
+    )
+    { month: easter_month, day: easter_day }
 end
+
+date = easter(2008)
+binding.pry
 
 def thanksgiving(do_year)
   november_thursdays = []
