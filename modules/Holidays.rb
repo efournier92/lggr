f8359594e1642be00dca9fb237f195b392b18441
efournier92 <efournier92@gmail.com>
2017-09-19 20:02:07 -0400

    move easter to Holidays

--- modules/Holidays.rb
+++ modules/Holidays.rb
@@ -1,3 +1,16 @@
 module Holidays
 
+  def easter(year)
+    epact_calc = ( 24 + 19 * ( year % 19 ) ) % 30
+    paschal_days = epact_calc - ( epact_calc / 28 )
+    days_to_sunday = paschal_days - (
+      ( year + ( year / 4 ) + paschal_days - 13 ) % 7
+    )
+    easter_month = 3 + ( days_to_sunday + 40 ) / 44
+    easter_day = days_to_sunday + 28 - (
+      31 * ( easter_month / 4 )
+    )
+    { month: easter_month, day: easter_day }
+  end
+
 end
