a52169a0a56a7378ef21efb3f691f1caa3bbcc85
efournier92 <efournier92@gmail.com>
2017-09-22 18:24:42 -0400

    add Good Friday logic

--- modules/Holidays.rb
+++ modules/Holidays.rb
@@ -22,15 +22,20 @@ module Holidays
     do_year = Holidays.add_easter(do_year)
     # Memorial Day
     find_last_occurance_in_month(do_year, 5, 'Mon', "[Memorial_Day]")
-
     # Independence Day
     do_year = Holidays.add_to_specific_date(do_year, 7, 4, "[Independence_Day]") 
     # Labor Day
+    do_year = Holidays.add_to_nth_day_in_month(do_year, 9, 1, 'Mon', "[Daylight_Saving(Begin)]")
     # Veterans Day
+    do_year = Holidays.add_to_specific_date(do_year, 11, 11, "[Veteran's_Day]") 
     # Daylight Savings (End)
-    do_year = Holidays.add_to_nth_day_in_month(do_year, 1, 11, 'Sun', "Daylight_Saving(Begin)")
+    do_year = Holidays.add_to_nth_day_in_month(do_year, 11, 1, 'Sun', "Daylight_Saving(Begin)")
     # Thanksgiving
+    do_year = Holidays.add_to_nth_day_in_month(do_year, 11, 4, 'Sun', "Thanksgiving")
     # Christmas
+    do_year = Holidays.add_to_specific_date(do_year, 12, 25, "[Christmas]") 
+    # New Year's Eve 
+    do_year = Holidays.add_to_specific_date(do_year, 12, 31, "[New_Year's_Eve]") 
   end
 
   def self.add_to_specific_date(do_year, month, month_day, holiday_name)
@@ -98,9 +103,23 @@ module Holidays
     easter_day = days_to_sunday + 28 - (
       31 * ( easter_month / 4 )
     )
+
+    good_friday_month = 
+      easter_day > 2 
+        ? easter_month
+        : easter_month - 1
+
+    good_friday_day = 
+      easter_day > 2 
+        ? easter_day - 2
+        : Year.days_in_months[good_friday_month - 1] - ( easter_day - 1 )
+
     do_year.weeks.each do | week |
       week.days.each do | day |
       if day.month == easter_month && day.month_day == easter_day
+        day.tasks.unshift('[Easter]')
+      end
+      if day.month == good_friday_month && day.month_day == good_friday_day
         day.tasks.unshift('[Easter]')
       end
     end
