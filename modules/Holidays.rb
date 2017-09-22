d8a13c34333d5222e4b2f205f686aa9b4fa081fa
efournier92 <efournier92@gmail.com>
2017-09-21 22:34:31 -0400

    start adding holiday statements

--- modules/Holidays.rb
+++ modules/Holidays.rb
@@ -10,30 +10,61 @@ module Holidays
     end
   end
 
-  def self.find_nth_day_in_month(do_year, month, nth_day, day_name, holiday_name)
+  def self.add_all(do_year)
+    # New Year's Day
+    do_year = Holidays.add_to_specific_date(do_year, 1, 1, "[New_Year's_Day]") 
+    # Presdent's Day
+    do_year = Holidays.add_to_nth_day_in_month(do_year, 2, 3, 'Mon', "President's_Day")
+    # Daylight Savings (Begin)
+    # Good Friday
+    # Easter
+    # Memorial Day
+    # Independence Day
+    # Labor Day
+    # Veterans Day
+    # Daylight Savings (End)
+    # Thanksgiving
+    # Christmas
+  end
+
+  def self.add_to_specific_date(do_year, month, month_day, holiday_name)
     day_count = 0
     do_year.weeks.each do | week |
       week.days.each do | day |
-        if day.month == month && day.name == day_name 
-          day_count += 1
-          if day_count == nth_day
-            day.tasks.unshift(holiday_name)
-          end
+      if day.month == month && day.month_day == month_day 
+        if day_count == nth_day
+          day.tasks.unshift(holiday_name)
         end
       end
+    end
     end
     do_year
   end
 
-  def self.find_last_day_in_month(do_year, month, day_name, holiday_name)
+  def self.add_to_nth_day_in_month(do_year, month, nth_day, day_name, holiday_name)
+    day_count = 0
     do_year.weeks.each do | week |
       week.days.each do | day |
-        last_week_in_month_start_date = days_in_months[2] - 6
-        if day.month == month && day.name == day_name 
-          && day.month_day >= last_week_in_month_start_date
+      if day.month == month && day.name == day_name 
+        day_count += 1
+        if day_count == nth_day
           day.tasks.unshift(holiday_name)
         end
       end
+    end
+    end
+    do_year
+  end
+
+  def self.find_last_occurance_in_month(do_year, month, day_name, holiday_name)
+    do_year.weeks.each do | week |
+      week.days.each do | day |
+      last_week_in_month_start_date = days_in_months[2] - 6
+      if day.month == month && day.name == day_name 
+        && day.month_day >= last_week_in_month_start_date
+        day.tasks.unshift(holiday_name)
+      end
+    end
     end
     do_year
   end
@@ -41,11 +72,11 @@ module Holidays
   def self.memorial_day(do_year)
     do_year.weeks.each do | week |
       week.days.each do | day |
-        if day.month == 5 && day.name == 'Mon' && day.month_day >= 25
-          day.tasks.unshift('[Memorial_Day]')
-          break
-        end
+      if day.month == 5 && day.name == 'Mon' && day.month_day >= 25
+        day.tasks.unshift('[Memorial_Day]')
+        break
       end
+    end
     end
     do_year
   end
@@ -63,10 +94,10 @@ module Holidays
     )
     do_year.weeks.each do | week |
       week.days.each do | day |
-        if day.month == easter_month && day.month_day == easter_day
-          day.tasks.unshift('[Easter]')
-        end
+      if day.month == easter_month && day.month_day == easter_day
+        day.tasks.unshift('[Easter]')
       end
+    end
     end
     do_year
   end
@@ -76,13 +107,13 @@ module Holidays
     november_thursdays_count = 0
     do_year.weeks.each do | week |
       week.days.each do | day |
-        if day.month == 11 && day.name == 'Thu'
-          november_thursdays_count += 1
-          if november_thursdays_count == 4
-            day.tasks.unshift('[Thanksgiving]')
-          end
+      if day.month == 11 && day.name == 'Thu'
+        november_thursdays_count += 1
+        if november_thursdays_count == 4
+          day.tasks.unshift('[Thanksgiving]')
         end
       end
+    end
     end
   end
 end
