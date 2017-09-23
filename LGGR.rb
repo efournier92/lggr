9c0e202fd7b9d12b457a3dbb00083b8ccec730ac
efournier92 <efournier92@gmail.com>
2017-09-23 19:34:21 -0400

    finish Year.add_holidays method

--- LGGR.rb
+++ LGGR.rb
@@ -14,42 +14,9 @@ do_year = Year.new(1999)
 
 Holidays.memorial_day(do_year)
 
-def easter(year)
-   epact_calc = ( 24 + 19 * ( year % 19 ) ) % 30
-    paschal_days = epact_calc - ( epact_calc / 28 )
-    days_to_sunday = paschal_days - (
-        ( year + ( year / 4 ) + paschal_days - 13 ) % 7
-    )
-    easter_month = 3 + ( days_to_sunday + 40 ) / 44
-    easter_day = days_to_sunday + 28 - (
-        31 * ( easter_month / 4 )
-    )
-    { month: easter_month, day: easter_day }
-end
-
-date = easter(2008)
-binding.pry
-
-def thanksgiving(do_year)
-  november_thursdays = []
-  november_thursdays_count = 0
-  do_year.weeks.each do |week|
-    week.days.each do |day|
-      if day.month == 11 && day.name == 'Thu'
-        november_thursdays_count += 1
-        if november_thursdays_count == 4
-          day.tasks.unshift('[Thanksgiving]')
-        end
-      end
-    end
-  end
-end
-
-thanksgiving = thanksgiving(do_year)
-binding.pry
-do_year = Year.month_start(do_year)
-do_year = Year.holidays(do_year)
-do_year = Year.birthdays(do_year)
+# do_year = Year.month_start(do_year)
+do_year.add_holidays
+do_year.add_birthdays
 
 binding.pry
 
