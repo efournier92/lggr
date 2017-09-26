b132f2036b2953e5e52e048714de7a788a463813
efournier92 <efournier92@gmail.com>
2017-09-26 17:57:43 -0400

    join print modules

--- modules/Lg.rb
+++ modules/Lg.rb
@@ -1,5 +1,26 @@
-module Lg
-  def self.print(do_year)
+module Printer
+  def self.print_do(do_year)
+    year = do_year.year 
+    out_file = File.new("./Out/#{ year }_DO.txt", "w")
+    do_year.weeks.each do | week |
+      week.days.each do | day |
+        if day.name == 'Mon' || ( day.month == 1 && day.month_day == 1 )
+          out_file.puts('************************')
+          out_file.puts("#{ year }-#{ '%02d' % day.month }-#{ '%02d' % day.month_day }")
+          out_file.puts('************************')
+        end
+        out_file.print("#{ day.name } - ")
+        day.tasks.each do |task|
+          out_file.print("#{ task }, ")
+        end
+        day.name == 'Sun' ?
+          out_file.puts("\n************************") :
+          out_file.puts('')
+      end
+    end
+  end
+
+  def self.print_lg(do_year)
     year = do_year.year
     out_file = File.new("./Out/#{ year }_LG.txt", "w")
 
@@ -22,10 +43,6 @@ module Lg
           end
       end
     end
-
-
-
-
   end
 end
 
