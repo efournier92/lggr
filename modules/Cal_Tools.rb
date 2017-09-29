d380b82ca1edf5592979d8378de3a8b28e00a2a0
efournier92 <efournier92@gmail.com>
2017-09-29 18:44:39 -0400

    refactor Holidays to accomodate relocated Cal_Tools functions

--- modules/Cal_Tools.rb
+++ modules/Cal_Tools.rb
@@ -1,24 +1,24 @@
 module Cal_Tools
-  def self.add_to_specific_date(do_year, month, month_day, holiday_name)
+  def self.add_to_specific_date(do_year, month, month_day, description_tag)
     day_count = 0
     do_year.weeks.each do | week |
         week.days.each do | day |
         if day.month == month && day.month_day == month_day 
-            day.tasks.unshift(holiday_name)
+            day.tasks.unshift(description_tag)
         end
       end
     end
     do_year
   end
 
-  def self.add_to_nth_day_in_month(do_year, month, nth_day, day_name, holiday_name)
+  def self.add_to_nth_day_in_month(do_year, month, nth_day, day_name, description_tag)
     day_count = 0
     do_year.weeks.each do | week |
       week.days.each do | day |
         if day.month == month && day.name == day_name 
           day_count += 1
           if day_count == nth_day
-            day.tasks.unshift(holiday_name)
+            day.tasks.unshift(description_tag)
           end
         end
       end
@@ -26,18 +26,17 @@ module Cal_Tools
     do_year
   end
 
-  def self.find_last_occurance_in_month(do_year, month, day_name, holiday_name)
+  def self.find_last_occurance_in_month(do_year, month, day_name, description_tag)
     do_year.weeks.each do | week |
       week.days.each do | day |
       last_week_in_month_start_date = Year.days_in_months[2] - 6
         if day.month == month && day.name == day_name && 
             day.month_day >= last_week_in_month_start_date
-          day.tasks.unshift(holiday_name)
+          day.tasks.unshift(description_tag)
         end
       end
     end
     do_year
   end
-
 end 
 
--- modules/Holidays.rb
+++ modules/Holidays.rb
@@ -2,19 +2,19 @@ module Holidays
 
   def self.add_all(do_year)
     # New Year's Day
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 1, 1, "[New_Year's_Day]"
     )
     # President's Day
-    do_year = Holidays.add_to_nth_day_in_month(
+    do_year = Cal_Tools.add_to_nth_day_in_month(
       do_year, 2, 3, 'Mon', "[President's_Day]"
     )
     # Valentine's Day
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 2, 14, "[Valentine's_Day]"
     )
     # Daylight Savings (Begin)
-    do_year = Holidays.add_to_nth_day_in_month(
+    do_year = Cal_Tools.add_to_nth_day_in_month(
       do_year, 3, 2, 'Sun', "[Daylight_Saving(Begin)]"
     )
     # Easter & Good Friday
@@ -22,77 +22,37 @@ module Holidays
       do_year
     )
     # Memorial Day
-    do_year = Holidays.find_last_occurance_in_month(
+    do_year = Cal_Tools.find_last_occurance_in_month(
       do_year, 5, 'Mon', "[Memorial_Day]"
     )
     # Independence Day
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 7, 4, "[Independence_Day]"
     )
     # Labor Day
-    do_year = Holidays.add_to_nth_day_in_month(
+    do_year = Cal_Tools.add_to_nth_day_in_month(
       do_year, 9, 1, 'Mon', "[Labor_Day]"
     )
     # Veterans Day
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 11, 11, "[Veterans_Day]"
     )
     # Daylight Savings (End)
-    do_year = Holidays.add_to_nth_day_in_month(
+    do_year = Cal_Tools.add_to_nth_day_in_month(
       do_year, 11, 1, 'Sun', "[Daylight_Saving(End)]"
     )
     # Thanksgiving
-    do_year = Holidays.add_to_nth_day_in_month(
+    do_year = Cal_Tools.add_to_nth_day_in_month(
       do_year, 11, 4, 'Sun', "[Thanksgiving]"
     )
     # Christmas
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 12, 25, "[Christmas]"
     )
     # New Year's Eve 
-    do_year = Holidays.add_to_specific_date(
+    do_year = Cal_Tools.add_to_specific_date(
       do_year, 12, 31, "[New_Year's_Eve]"
     )
-  end
-
-  def self.add_to_specific_date(do_year, month, month_day, holiday_name)
-    day_count = 0
-    do_year.weeks.each do | week |
-        week.days.each do | day |
-        if day.month == month && day.month_day == month_day 
-            day.tasks.unshift(holiday_name)
-        end
-      end
-    end
-    do_year
-  end
-
-  def self.add_to_nth_day_in_month(do_year, month, nth_day, day_name, holiday_name)
-    day_count = 0
-    do_year.weeks.each do | week |
-      week.days.each do | day |
-        if day.month == month && day.name == day_name 
-          day_count += 1
-          if day_count == nth_day
-            day.tasks.unshift(holiday_name)
-          end
-        end
-      end
-    end
-    do_year
-  end
-
-  def self.find_last_occurance_in_month(do_year, month, day_name, holiday_name)
-    do_year.weeks.each do | week |
-      week.days.each do | day |
-      last_week_in_month_start_date = Year.days_in_months[2] - 6
-        if day.month == month && day.name == day_name && 
-            day.month_day >= last_week_in_month_start_date
-          day.tasks.unshift(holiday_name)
-        end
-      end
-    end
-    do_year
   end
 
   def self.add_easter_and_good_friday(do_year)
