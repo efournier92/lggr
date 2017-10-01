de56078408a44f7a91cf77ce114f70f3d19030df
efournier92 <efournier92@gmail.com>
2017-10-01 12:57:28 -0400

    change description_tag to new_tag

--- modules/Add_Tag.rb
+++ modules/Add_Tag.rb
@@ -1,25 +1,25 @@
 module Add_Tag 
 
-  def self.to_specific_date(do_year, month, month_day, description_tag)
+  def self.to_specific_date(do_year, month, month_day, new_tag)
     day_count = 0
     do_year.weeks.each do | week |
         week.days.each do | day |
         if day.month == month && day.month_day == month_day 
-            day.tasks.unshift(description_tag)
+            day.tasks.unshift(new_tag)
         end
       end
     end
     do_year
   end
 
-  def self.to_nth_xday_in_month(do_year, month, nth_day, day_name, description_tag)
+  def self.to_nth_xday_in_month(do_year, month, nth_day, day_name, new_tag)
     day_count = 0
     do_year.weeks.each do | week |
       week.days.each do | day |
         if day.month == month && day.name == day_name 
           day_count += 1
           if day_count == nth_day
-            day.tasks.unshift(description_tag)
+            day.tasks.unshift(new_tag)
           end
         end
       end
@@ -27,22 +27,22 @@ module Add_Tag
     do_year
   end
 
-  def self.to_last_week_in_month(do_year, month, day_name, description_tag)
+  def self.to_last_week_in_month(do_year, month, day_name, new_tag)
     do_year.weeks.each do | week |
       week.days.each do | day |
       last_week_in_month_start_date = Year.days_in_months[2] - 6
         if day.month == month && day.name == day_name && 
             day.month_day >= last_week_in_month_start_date
-          day.tasks.unshift(description_tag)
+          day.tasks.unshift(new_tag)
         end
       end
     end
     do_year
   end
 
-  def self.to_last_day_of_month(do_year, month, description_tag)
+  def self.to_last_day_of_month(do_year, month, new_tag)
     last_day_of_month = Month.days_in_months[month - 1]
-    Add_Tag.to_specific_date(do_year, month, description_tag)
+    Add_Tag.to_specific_date(do_year, month, new_tag)
   end
 
 end 
