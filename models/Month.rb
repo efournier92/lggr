e8c3598015560957814eb4b8696a70c19fc31380
efournier92 <efournier92@gmail.com>
2017-10-01 12:57:04 -0400

    prep OO rewrite for final refactor

--- models/Month.rb
+++ models/Month.rb
@@ -6,12 +6,14 @@ class Month
 
   def self.add_start_tasks(do_year, week_index, days_this_week, month)
     for (month = 1; month <= 12; month += 1) 
-      Add_Tag.to_specific_date(do_year.year, month, 1, 
-                               'ByTrnTk, PyIntrnt, PyRnt, RcvScrpts, ')
-      Add_Tag.to_nth_day_in_month(do_year.year, month, 1, 'Sat', 
+      Add_Tag.to_last_day_of_month(do_year, month, 
+                                   'Rnt_PrpChk, ')
+      Add_Tag.to_specific_date(do_year, month, 1, 
+                               'ByTrnTk, Rnt_Py, PyIntrnt, ')
+      Add_Tag.to_nth_day_in_month(do_year, month, 1, 'Sat', 
                                   'Scpts_Ord, Scpts_Rcv, Scpts_CtPls, ')
-      Add_Tag.to_nth_day_in_month(do_year.year, month, 1, 'Sun', 
-                                  'NxBk, FrsRzrs, FrshCntcts, ')
+      Add_Tag.to_nth_day_in_month(do_year, month, 1, 'Sun', 
+                                  'FrshCntcts, FrshRzrs, NxBk')
       quarter_months = [1, 4, 7, 10]
       quarter_months.each do |quarter_month|
         if (month == quarter_month)
