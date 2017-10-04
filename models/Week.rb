27cb127deff93299fa2a338fa2ba66e1c28427d7
efournier92 <efournier92@gmail.com>
2017-10-03 21:02:32 -0400

    finish refactoring Year.rb

--- models/Week.rb
+++ models/Week.rb
@@ -1,46 +1,31 @@
 class Week
-  attr_accessor :week_index, :days_this_week, :days
+  attr_accessor :week_index, :days_this_week
 
   def initialize(week_index, days_this_week, month)
     @week_index = week_index
     @days_this_week = days_this_week
-    @days = Week.template(month, days_this_week)
   end
 
-  def self.template(month, days_this_week)
-    mon = Day.new('Mon', ['Gt', 'Af_Rcv(1900)'], month, days_this_week[0])
-    month += 1 if days_this_week[0] > days_this_week[1]
-    tue = Day.new('Tue', ['Gt'], month, days_this_week[1])
-    month += 1 if days_this_week[1] > days_this_week[2]
-    wed = Day.new('Wed', ['Gt'], month, days_this_week[2])
-    month += 1 if days_this_week[2] > days_this_week[3]
-    thu = Day.new('Thu', ['Gt'], month, days_this_week[3])
-    month += 1 if days_this_week[3] > days_this_week[4]
-    fri = Day.new('Fri', 
-                 ['Gt', 'aLg', 'Lg', 'FnLg[R]s', 'LgWk', 'Bgt', 'PyCC'],
-                 month, days_this_week[4])
-    month += 1 if days_this_week[4] > days_this_week[5]
-    sat = Day.new('Sat', ['Gt', 'Af_Ord()'], month, days_this_week[5])
-    month += 1 if days_this_week[5] > days_this_week[6]
-    sun = Day.new('Sun', 
-                  ['Gt', 'Amz()', 'ClnMbDsktp', 'TM', 'Ln', 'ClHm(1300, )', 
-                   'Vac()', 'ClnKtch', 'ClnFrdg', 'Sv', 'Plk', 'Ns', 'Ap'],
-                  month, days_this_week[6])
-    [mon, tue, wed, thu, fri, sat, sun]
+  def self.days
+    { 'Mon' => ['Gt', 'Af_Rcv(1900)'],
+      'Tue' => ['Gt'],
+      'Wed' => ['Gt'],
+      'Thu' => ['Gt'],
+      'Fri' => ['Gt', 'aLg', 'Lg', 'FnLg[R]s', 'LgWk', 'Bgt', 'PyCC'],
+      'Sat' => ['Gt', 'Af_Ord()'],
+      'Sun' => ['Gt', 'Amz()', 'ClnMbDsktp', 'TM', 'Ln', 'ClHm(1300, )', 
+                'Vac()', 'ClnKtch', 'ClnFrdg', 'Sv', 'Plk', 'Ns', 'Ap']
+    }
   end
 
-  def self.days_this_week(day, month)
-    day_array = []
-    days_in_month = Templates.days_in_months[month-1]
-    7.times do
-      if day > days_in_month
-        day = 1
-      end
-      day_array.push(day)
-      day += 1
+  def self.build(month, days_this_week)
+    week_arr = []
+    Week.days.each_with_index do | day_name, day_tasks |
+      day = Day.new(day_name, day_tasks, month, days_this_week[index])
+      week_arr.push(day)
     end
-    day_array
+    week_arr
   end
-  
+
 end
 
--- models/Year.rb
+++ models/Year.rb
@@ -3,32 +3,30 @@ class Year
   attr_reader :year
 
   def initialize(year)
-    @year = year
+    @year  = year
     @weeks = []
-    month = 1
+    month  = 1
     week_index = 1
     days_in_months = Year.days_in_months
-
+    # adjust for leap year
     days_in_months[2] = 29 if self.leap_year?
-    day = Cal_Tools.get_first_monday(year)
+    day = Year.get_first_monday(year)
+
+    # TODO: fill in last at start & end of year 
 
     52.times do
       days_this_week = Week.days_this_week(day, month)
       do_week = Week.new(week_index, days_this_week, month)
-
       if do_week.week_index.odd?
-        do_week.days.find do |day|
+        do_week.days.find do | day |
           if day.name == 'Sun'
             day.tasks = Day.odd_sunday_tasks 
           end
         end
       end
-
-      # do_week = Cal_Tools.month_end(do_week, do_week.days)
-      # do_week = Cal_Tools.month_start(do_week, week_index, do_week.days, month)
-
       @weeks.push(do_week)
       day += 7
+      # check if new month
       if day > days_in_months[month - 1]
         day = day - days_in_months[month - 1]
         month += 1
@@ -46,33 +44,31 @@ class Year
     @year % 4 == 0 && !( @year % 100 == 0 && @year % 400 != 0 )
   end
 
+  def self.get_first_monday(year)
+    years_since = year - 1
+    leap_years = years_since / 4
+    century_years = years_since / 100
+    four_century_years = years_since / 400
+
+    total_leap_years = leap_years - century_years + four_century_years
+    total_precesion = years_since + total_leap_years
+    day_index = total_precesion % 7
+
+    first_monday = 8 - day_index;
+
+    if first_monday > 7
+      first_monday = first_monday - 7
+    end
+    first_monday
+  end
+
   def self.add_holidays(do_year)
     Holidays.add_all(do_year)
   end
 
   def self.add_birthdays(do_year)
-    do_year.weeks.each do |week|
-      week.days.each do |day|
-        if day.month == 9 && day.month_day == 29
-          day.tasks.unshift("[Bday_Dd]")
-        elsif day.month == 8 && day.month_day == 26
-          day.tasks.unshift("[Bday_Mm]")
-        elsif day.month == 8 && day.month_day == 15
-          day.tasks.unshift("[Bday_Kevin]")
-        elsif day.month == 8 && day.month_day == 9
-          day.tasks.unshift("[Bday_Lori]")
-        elsif day.month == 10 && day.month_day == 10
-          day.tasks.unshift("[Bday_Brian]")
-        elsif day.month == 2 && day.month_day == 13
-          day.tasks.unshift("[Bday_Eric]")
-        elsif day.month == 8 && day.month_day == 19
-          day.tasks.unshift("[Bday_Nate]")
-        elsif day.month == 11 && day.month_day == 28
-          day.tasks.unshift("[Bday_Grant]")
-        end
-      end
-    end
-    do_year
+    Birthdays.add_all(do_year)
   end
+
 end
 
