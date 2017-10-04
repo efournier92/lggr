37367586e354aa9963a8148edb50e6af89c9c2d6
efournier92 <efournier92@gmail.com>
2017-10-04 17:58:56 -0400

    finish refactoring LGGR.rb

--- LGGR.rb
+++ LGGR.rb
@@ -10,9 +10,6 @@ require 'pry'
 
 print_type = ''
 month = 1
-binding.pry
-
-days_in_months = do_year.days_in_months
 
 until type == 'DO' || type == 'LG'
   print "DO || LG?\n>> "
@@ -26,14 +23,10 @@ year = gets.chomp.to_i
 do_year = Year.new(year)
 # add monthly tasks to year object
 do_year = Month.add_start_tasks(do_year)
-# add holidays to year object
-do_year = Year.add_holidays(do_year)
 # add birthdays to year object
 do_year = Year.add_birthdays(do_year)
-
-if Cal_Tools.is_leap_year?(year)
-  days_in_months[1] = 29
-end
+# add holidays to year object
+do_year = Year.add_holidays(do_year)
 
 if type == 'DO'
   Printer.print_do(do_year)
