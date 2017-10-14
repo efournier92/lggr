ad0738dad8adf8975949df2a7280422be57b6de1
efournier92 <efournier92@gmail.com>
2017-10-14 15:08:12 -0400

    finish 4 week shifting methods

--- modules/Bookend_Weeks.rb
+++ modules/Bookend_Weeks.rb
@@ -1,18 +1,35 @@
 module Bookend_Weeks
 
   def self.shift_do_start(do_year)
-    until do_year.weeks.first.days.any? { | day | day.month == 1 }
+    while do_year.weeks.first.days.all? { | day | day.month == 12 }
       do_year.weeks.shift
     end
     do_year
   end
 
+  def self.shift_lg_start(do_year)
+    while do_year.weeks.first.days.all? { | day | day.month == 12 }
+      do_year.weeks.shift
+    end
+    do_year.weeks.first.days.shift(5)
+    do_year
+  end
+
   def self.shift_do_end(do_year)
-    until do_year.weeks.last.days.all? { | day | day.month == 12 }
+    while do_year.weeks.last.days.all? { | day | day.month == 1 }
       do_year.weeks.pop
     end
     do_year
   end
+
+  def self.shift_lg_end(do_year)
+    while do_year.weeks.last.days.all? { | day | day.month == 1 }
+      do_year.weeks.pop
+    end
+    do_year.weeks.last.days.pop(2)
+    do_year
+  end
+
   # def self.shift_do_end(do_year)
   #   # index of Monday if 'DO', Friday otherwise
   #   day_index = print_type == 'DO' ? 0 : 4
