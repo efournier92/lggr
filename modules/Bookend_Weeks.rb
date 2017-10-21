6fc0355812204623f96d81c10e11ee7bb7e67c5e
efournier92 <efournier92@gmail.com>
2017-10-20 22:05:23 -0400

    add missing holidays

---
+++ modules/Bookend_Weeks.rb
@@ -0,0 +1,40 @@
+module Bookend_Weeks
+
+  def self.shift_do_start(do_year)
+    while do_year.weeks.first.days.all? { | day | day.month == 12 }
+      do_year.weeks.shift
+    end
+    do_year
+  end
+
+  def self.shift_lg_start(do_year)
+    until do_year.weeks.first.days.any? { | day | day.month == 1 }
+      first_day = do_year.weeks.first.days.first.month_day
+      first_day_month = do_year.weeks.first.days.first.month
+      break if first_day_month == 12 && first_day >= 21
+      do_year.weeks.shift 
+    end
+    do_year.weeks.first.days.shift(5)
+    do_year
+  end
+
+  def self.shift_do_end(do_year)
+    while do_year.weeks.last.days.all? { | day | day.month == 1 }
+      do_year.weeks.pop
+    end
+    do_year
+  end
+
+  def self.shift_lg_end(do_year)
+    while do_year.weeks.last.days.all? { | day | day.month == 1 }
+      first_day = do_year.weeks.last.days.first.month_day
+      last_day_month = do_year.weeks.last.days.last.month
+      break if last_day_month == 1 && first_day == 1
+      do_year.weeks.pop
+    end
+    do_year.weeks.last.days.pop(2)
+    do_year
+  end
+
+end
+
