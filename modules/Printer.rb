425ceb7904f57e32ac0b3bd8186d06d1a9300e8e
efournier92 <efournier92@gmail.com>
2017-12-22 11:20:42 -0500

    start converting Printer.Lg module to md syntax

--- modules/Printer.rb
+++ modules/Printer.rb
@@ -20,28 +20,26 @@ module Printer
 
   def self.print_lg(do_year)
     year = do_year.year
-    out_file = File.new("./Out/LG_#{ '%04d' % year }.txt", "w")
+    out_file = File.new("./Out/LG_#{ '%04d' % year }.md", "w")
 
-    weekday_string = "***\n[S] \n***\n[R] \n"
-    weekend_string = "***\n[R] \n"
-    friday_string  = "***\n[S] \n***\n[S] \n***\n[R] \n"
+    weekday_string = "### Tsk\n###Scrum \n### \n"
+    weekend_string = "### Tsk\n###\n"
+    friday_string  = "### Tsk\n### ScrumNext\n### Scrum\n### \n"
 
     do_year.weeks.reverse_each do | week |
       week.days.reverse_each do | day |
-        out_file.puts('************************')
-        out_file.puts("#{ '%04d' % day.year }-#{ '%02d' % day.month }-#{ '%02d' % day.month_day }")
-        out_file.puts('***********')
-        out_file.puts("#{ day.name } - ")
-        if day.name == 'Fri'
+      out_file.puts("### #{ '%04d' % day.year }-#{ '%02d' % day.month }-#{ '%02d' % day.month_day } | #{day.name}")
+        if day.name == 'Friday'
           out_file.puts(friday_string)
-        elsif day.name == 'Sat' || day.name == 'Sun'
+        elsif day.name == 'Saturday' || day.name == 'Sunday'
           out_file.puts(weekend_string)
         else
           out_file.puts(weekday_string)
         end
       end
+    out_file.puts
     end
-    out_file.puts('************************')
+    out_file.puts
   end
 
 end
