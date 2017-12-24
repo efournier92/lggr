1980cc0ac6da6ea8c7910315084ffb64653384aa
efournier92 <efournier92@gmail.com>
2017-12-24 11:37:21 -0500

    update Rnt monthly tasks

--- models/Month.rb
+++ models/Month.rb
@@ -11,7 +11,7 @@ class Month
                                'TrnTk_By')
       # last day of month
       do_year = Add_Tag.to_last_day_of_month(do_year, month, 
-                                             'RntPrepChk')
+                                             'Rnt_Chk_Pk')
       # first day of month
       do_year = Add_Tag.to_specific_date(do_year, month, 1, 
                                'Rnt_Py, Orbt_NwLst, Lg_DoLst_NxtMnth')
