939f645a50fb345ed7cbcc3ca67bacd798e8c0cc
efournier92 <efournier92@gmail.com>
2018-01-13 11:44:21 -0500

    add input validations

--- LGGR.rb
+++ LGGR.rb
@@ -9,7 +9,7 @@ require './modules/Bookend_Weeks'
 require './modules/Holidays'
 require './modules/Printer'
 print_type = ''
-print_month
+print_month = nil
 month = 1
 
 until print_type == 'DO' || print_type == 'LG'
@@ -20,18 +20,22 @@ end
 if print_type == 'DO'
   is_valid = false
   until is_valid == true
-    print "MONTH: All || [0-11]\n>> "
+    print "MONTH: All || [1-12]\n>> "
     print_month = gets.chomp
     is_valid = true if print_month == 'all' 
     if print_month.match(/^([1-9]|[01][0-1])$/)
       is_valid = true 
       print_month = print_month.to_i
+      print_month += 1
     end
   end
 end
 
-print "Which Year?\n>> "
-year = gets.chomp.to_i
+year = nil
+until year.is_a? Integer
+  print "Which Year?\n>> "
+  year = gets.chomp.to_i
+end
 
 # create year object
 do_year = Year.new(year)
@@ -47,9 +51,9 @@ if print_type == 'DO'
   do_year = Bookend_Weeks.shift_do_start(do_year)
   do_year = Bookend_Weeks.shift_do_end(do_year)
   if print_month
-    Printer.print_do(do_year)
-  else
     Printer.print_do_month(do_year, print_month)
+  else
+    Printer.print_do_year(do_year)
   end
 elsif print_type == 'LG'
   do_year = Bookend_Weeks.shift_lg_start(do_year)
