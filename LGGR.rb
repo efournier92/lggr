8596693b03bcece2e83814d92d7b3d628781b2b4
efournier92 <efournier92@gmail.com>
2018-03-02 19:35:50 -0500

    add comments to lggr.rb

--- LGGR.rb
+++ LGGR.rb
@@ -12,19 +12,20 @@ print_type = ''
 print_month = nil
 month = 1
 
+# Collect type user input
 until print_type == 'DO' || print_type == 'LG'
   print "DO || LG\n>> "
   print_type = gets.chomp.upcase
 end
 
-
+# Collect year user input
 year = nil
 until year.is_a? Integer
   print "Which Year?\n>> "
   year = gets.chomp.to_i
 end
 
-
+# Collect month user input
 if print_type == 'DO'
   is_valid = false
   until is_valid == true
