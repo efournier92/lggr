d53496d063bf48d336e91bb0c0819a862284be3f
misigno <efournier92@gmail.com>
2018-06-04 07:19:05 -0400

    remove laundry offset logic

--- LGGR.rb
+++ LGGR.rb
@@ -47,17 +47,6 @@ do_year = Month.add_start_tasks(do_year)
 do_year = Year.add_birthdays(do_year)
 # add annual tasks to year object
 do_year = Year.add_annual_tasks(do_year)
-# add laundry tasks with offset
-if print_type == 'DO'
-  ln_offset = nil
-  until ln_offset.is_a? Integer
-    print "Laundry Offset [0-2]\n>> "
-    ln_offset = gets.chomp.to_i
-  end
-  do_year = Add_Tag.to_xday_every_n_weeks(
-    do_year, 3, "Sunday", "Ln_drmt, Ln_Fld", ln_offset
-  )
-end
 
 if print_type == 'DO'
   do_year = Bookend_Weeks.shift_do_start(do_year)
